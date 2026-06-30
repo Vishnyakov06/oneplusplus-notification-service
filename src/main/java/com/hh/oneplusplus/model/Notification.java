8dd2fd67acd49e1ed63cda756f1c77332f16dfcf
Danya <danya.vishnyakov.2006@mail.ru>
2026-06-30 18:42:05 +0300

    добавлены изменения, liquibase теперь поднимаетсят

--- src/main/java/com/hh/oneplusplus/model/Notification.java
+++ src/main/java/com/hh/oneplusplus/model/Notification.java
@@ -17,7 +17,7 @@ import java.util.Map;
 import java.util.UUID;
 
 @Entity
-@Table(name = "notification")
+@Table(name = "notifications")
 public class Notification {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
