31dcfc1587dec36daab1bec2caafd733a07156bc
Vishnyakov06 <147653545+Vishnyakov06@users.noreply.github.com>
2026-07-14 21:38:16 +0300

    one-100: Исправление invite уведомления (#5)
    * исправлен баг с отсутсвтием ссылки при инвайте на почту
    
    * изменил подход, изменена схема message-template

--- src/main/java/com/hh/oneplusplus/repository/MessageTemplateRepository.java
+++ src/main/java/com/hh/oneplusplus/repository/MessageTemplateRepository.java
@@ -1,6 +1,7 @@
 package com.hh.oneplusplus.repository;
 
 import com.hh.oneplusplus.dto.notification.NotificationEventType;
+import com.hh.oneplusplus.dto.notification.NotificationType;
 import com.hh.oneplusplus.model.MessageTemplate;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,7 +9,6 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface MessageTemplateRepository
-        extends JpaRepository<MessageTemplate, NotificationEventType> {
-    Optional<MessageTemplate> findByEventType(NotificationEventType type);
+public interface MessageTemplateRepository extends JpaRepository<MessageTemplate, Long> {
+    Optional<MessageTemplate> findByEventTypeAndChannel(NotificationEventType eventType, NotificationType channel);
 }
