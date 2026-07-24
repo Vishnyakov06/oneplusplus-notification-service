07408aa434e79a634ff8e88221ee38ed36dca6bf
Vishnyakov06 <147653545+Vishnyakov06@users.noreply.github.com>
2026-07-24 12:45:29 +0300

    One 108: Добавлены уведомления для event-request (#6)
    * добавил новые шаблоны для новых типов

---
+++ src/main/resources/db/changelog/changes/008-insert-join-request-templates.sql
@@ -0,0 +1,4 @@
+INSERT INTO message_templates (event_type, channel, template) VALUES
+    ('JOIN_REQUEST_SUBMITTED', 'WEB', '${actor.name} ${actor.surname} подал заявку на участие в событии "${eventTitle}"'),
+    ('JOIN_REQUEST_APPROVED', 'WEB', 'Ваша заявка на участие в событии "${eventTitle}" одобрена'),
+    ('JOIN_REQUEST_REJECTED', 'WEB', 'Ваша заявка на участие в событии "${eventTitle}" отклонена. Причина: ${comment}');
