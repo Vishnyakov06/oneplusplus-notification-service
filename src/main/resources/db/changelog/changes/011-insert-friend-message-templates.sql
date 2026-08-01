82ab45a792f5d5a261ba28d349c48d6bdc323bbf
mrskeletone <mr_skeletone_1000@mail.ru>
2026-08-01 20:08:11 +0300

    Друзья

--- src/main/resources/db/changelog/changes/011-insert-friend-message-templates.sql
+++ src/main/resources/db/changelog/changes/011-insert-friend-message-templates.sql
@@ -1,4 +1,6 @@
 INSERT INTO message_templates (event_type, channel, template) VALUES
-     ('ADD_FRIEND','WEB',  'Пользователь ${userName} ${userSurname} отправил приглашение в друзья}'),
-     ('DELETE_FRIEND','WEB',  'Пользователь ${userName} ${userSurname} удалил вас из друзей}'),
-     ('CREATE_POST','WEB',  'Ваш друг ${userName} ${userSurname} создал новый пост!}')
+     ('ADD_FRIEND','WEB',  '${userName} ${userSurname} отправил приглашение в друзья}'),
+     ('DELETE_FRIEND','WEB',  '${userName} ${userSurname} удалил вас из друзей}'),
+     ('CREATE_POST','WEB',  'Ваш друг ${userName} ${userSurname} создал новый пост!}'),
+     ('ACCEPT_FRIEND','WEB',  '${userName} ${userSurname} принял ваш запрос в друзья!}'),
+     ('REJECT_FRIEND','WEB',  '${userName} ${userSurname} отклонил ваш запрос в друзья!}')
